41a794d8d9415a33a4b56ebec1348e6cd2e7798f
TurchaninovStudent <aeturchaninov@ya.ru>
2026-02-14 13:45:30 +0300

    commit on main

--- app/src/main/java/com/example/turchaninov/visitcard/MainActivity.kt
+++ app/src/main/java/com/example/turchaninov/visitcard/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -157,7 +158,7 @@ fun VisitCardInfo(
     ) {
         Spacer(modifier)
         PersonInfo(
-            fullName = "Турчанинов Андрей Евгеньевич",
+            fullName = stringResource(R.string.full_name_text),
             title = "создал это приложение"
         )
         SocialInfoGroup(
